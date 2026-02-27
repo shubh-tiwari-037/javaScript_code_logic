d992a2ab98d076e1c5274277b3894aa6407cc439
shubh-tiwari-037 <shubhtiwari037@gmail.com>
2026-02-27 15:49:04 +0530

    same char same line me

--- Patterns/Pratic.js
+++ Patterns/Pratic.js
@@ -48,10 +48,19 @@
 // numchange(n);
 
 // -----------Number increased Reverse pyramid
+// let n=5
+// for(let i=1; i<=n; i++){
+//     for(let j=1; j<=n-i+1; j++){
+//         process.stdout.write(j.toString())
+//     }
+//     console.log()
+// }
+
+// ------------number trangular par abhi chala nahi h
 let n=5
 for(let i=1; i<=n; i++){
-    for(let j=1; j<=n-i+1; j++){
-        process.stdout.write(j.toString())
+    for(let j=1; j<=i; j++){
+        process.stdout.write(i.toString())
     }
     console.log()
 }
