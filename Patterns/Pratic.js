bcd8325a2b880508248a4d0bced5ab8d6171d09d
shubh-tiwari-037 <shubhtiwari037@gmail.com>
2026-02-27 15:35:19 +0530

    revers increment

--- Patterns/Pratic.js
+++ Patterns/Pratic.js
@@ -46,3 +46,12 @@
 // }
 // }
 // numchange(n);
+
+// -----------Number increased Reverse pyramid
+let n=5
+for(let i=1; i<=n; i++){
+    for(let j=1; j<=n-i+1; j++){
+        process.stdout.write(j.toString())
+    }
+    console.log()
+}
